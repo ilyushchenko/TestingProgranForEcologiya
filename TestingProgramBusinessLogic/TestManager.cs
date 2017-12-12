ddfebfa01eecfb233c1577f511e0382553a8a396
Turchak411 <t-a-a-96@mail.ru>
2017-12-12 21:50:58 +0300

    Создание структуры менеджера

--- TestingProgramBusinessLogic/TestManager.cs
+++ TestingProgramBusinessLogic/TestManager.cs
@@ -11,10 +11,35 @@ namespace TestingProgramBusinessLogic
         //TODO: Prop:Try count
         //TODO: Prop:Свойство количество тиков
 
+        private int m_tryCount;
+        private int m_finishTime;
+
+        private TestManager() {  }
+        
+        public TestManager (int tryCount, int finishTime)
+	    {
+            m_tryCount = tryCount;
+            m_finishTime = finishTime;
+	    }
+
         //TODO: Старт варианта () -> Time <- CurrentTime + 45 min ==> Variant 
+        public void StartTest()
+        {
+
+        }
+
         //TODO: Tick() <- 
         //TODO: Метод проверки окончания () <- Bool 
+        public bool IsTestFinished(int currentTime)
+        {
+            return currentTime >= m_finishTime ? true : false;
+        }
+
         //TODO: Метод ввода ответа
+        public double GetAnwser()
+        {
+
+        }
         
     }
 }
