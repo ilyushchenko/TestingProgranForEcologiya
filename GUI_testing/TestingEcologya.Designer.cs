a1f7b4ca391c8dbfdf85c1fd215cc6e87548f8fd
Ande5 <33572132+Ande5@users.noreply.github.com>
2017-12-12 21:18:47 +0300

    ex7

--- GUI_testing/TestingEcologya.Designer.cs
+++ GUI_testing/TestingEcologya.Designer.cs
@@ -30,15 +30,78 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControlEcologya = new System.Windows.Forms.TabControl();
+            this.tabPageBook = new System.Windows.Forms.TabPage();
+            this.tabPageTester = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControlEcologya.SuspendLayout();
+            this.tabPageBook.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // TestingEcologiya
+            // tabControlEcologya
+            // 
+            this.tabControlEcologya.Controls.Add(this.tabPageBook);
+            this.tabControlEcologya.Controls.Add(this.tabPageTester);
+            this.tabControlEcologya.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlEcologya.Location = new System.Drawing.Point(0, 0);
+            this.tabControlEcologya.Name = "tabControlEcologya";
+            this.tabControlEcologya.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabControlEcologya.SelectedIndex = 0;
+            this.tabControlEcologya.Size = new System.Drawing.Size(771, 383);
+            this.tabControlEcologya.TabIndex = 0;
+            // 
+            // tabPageBook
+            // 
+            this.tabPageBook.Controls.Add(this.splitContainer1);
+            this.tabPageBook.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBook.Name = "tabPageBook";
+            this.tabPageBook.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBook.Size = new System.Drawing.Size(763, 357);
+            this.tabPageBook.TabIndex = 0;
+            this.tabPageBook.Text = "Электронный учебник";
+            this.tabPageBook.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTester
+            // 
+            this.tabPageTester.Location = new System.Drawing.Point(4, 22);
+            this.tabPageTester.Name = "tabPageTester";
+            this.tabPageTester.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageTester.Size = new System.Drawing.Size(763, 357);
+            this.tabPageTester.TabIndex = 1;
+            this.tabPageTester.Text = "Тестирование";
+            this.tabPageTester.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(757, 351);
+            this.splitContainer1.SplitterDistance = 184;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // TestingEcologya
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 383);
-            this.Name = "TestingEcologiya";
+            this.Controls.Add(this.tabControlEcologya);
+            this.Name = "TestingEcologya";
             this.Text = "Тестирование по Экологии";
+            this.tabControlEcologya.ResumeLayout(false);
+            this.tabPageBook.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -46,6 +109,10 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControlEcologya;
+        private System.Windows.Forms.TabPage tabPageBook;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabPage tabPageTester;
     }
 }
 
